ffe4b988008403442a5e06eb5d1f81d6acffdb1e
Jorge Navarrete <giomaussi@gmail.com>
2024-04-22 09:44:09 -0500

    minor change

--- LaShuffle.py
+++ LaShuffle.py
@@ -6,5 +6,11 @@ gente: list[str] = ['Tom', 'Claudio', 'Martin', 'Diana', 'Inka' , 'Daphne']
 
 for G in range(Iteraciones):
     shuffle(gente)
+    #print(gente)
+    G += 1
+    
+
+for G in range(gente.__len__()):
+    shuffle(gente)
     print(gente)
     G += 1
